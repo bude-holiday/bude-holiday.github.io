f2cdec73c21c3e32154758e52ab3dcaf95581459
Matt Rayner <me@mattihew.com>
2021-07-23 02:25:09 +0100

    updated sed command to actually replace files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "URL is: ${URL}";
 if [[ -z "${URL}" ]]; then
-    sed "s/^url:.*/url: ${URL}/g" _config.yml > _config.yml
+    echo "updating url to ${URL}"
+    sed -i "s/^url:.*/url: ${URL}/g" _config.yml
 fi
 bundle exec jekyll build
