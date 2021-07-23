4d61082cb93932561c3dfd9cc065e64cf73a584f
Matt Rayner <me@mattihew.com>
2021-07-23 23:59:23 +0100

    made git branch configurable

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [[ -n "${URL}" ]]; then
-    echo "updating url to ${URL}"
-    sed -i "s#^url:.*#url: ${URL}#g" _config.yml
+    echo "updating url to ${URL}";
+    sed -i "s#^url:.*#url: ${URL}#g" _config.yml;
 fi
-bundle exec jekyll build
+if [[ -n "${BRANCH}" ]]; then
+    echo "updated branch to ${BRANCH}";
+    sed -i "s#^branch:.*#branch: ${BRANCH}#g" _config.yml;
+fi
+bundle exec jekyll build;
