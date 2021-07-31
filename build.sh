431e7f9a1479fc48526ff3dd6787227a523a99fc
Matt Rayner <me@mattihew.com>
2021-07-31 15:18:36 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-if [[ -n "${URL}" ]]; then
-    echo "updating url to ${URL}";
-    sed -i "s#^url:.*#url: ${URL}#g" _config.yml;
+echo "Deploy prime url: ${DEPLOY_PRIME_URL}"
+if [ "${CONTEXT}" == "production" ]; then
+    if [[ -n "${URL}" ]]; then
+        echo "updating url to ${URL}";
+        sed -i "s#^url:.*#url: ${URL}#g" _config.yml;
+    fi
+else
+    if [[ -n "${DEPLOY_PRIME_URL}" ]]; then
+        echo "updating url to ${DEPLOY_PRIME_URL}";
+        sed -i "s#^url:.*#url: ${DEPLOY_PRIME_URL}#g" _config.yml;
+    fi
 fi
+
 if [[ -n "${BRANCH}" ]]; then
-    echo "updated branch to ${BRANCH}";
+    echo "updating branch to '${BRANCH}'";
     sed -i "s#^branch:.*#branch: ${BRANCH}#g" _config.yml;
 fi
 bundle exec jekyll build;
